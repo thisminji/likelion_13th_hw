79835e4115a19783ab88173bf76b14fb3599553a
thisminji <thisminji@dgu.ac.kr>
2025-04-09 18:14:40 +0900

    2주차 과제 이미지 수정

--- project/project/settings.py
+++ project/project/settings.py
@@ -118,6 +118,9 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
